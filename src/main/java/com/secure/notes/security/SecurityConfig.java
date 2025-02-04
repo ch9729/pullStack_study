f8ba98e01364d3d52dcde6af98383ec580ffe0b9
ch9729 <karlll@naver.com>
2025-02-04 14:57:36 +0900

    시큐리티 설정 및 디펜던시 추가

---
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.secure.notes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean   //Configuration 필수
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(((requests) ->
+                requests
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()));
+        //http.formLogin(withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
