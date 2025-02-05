7723d67947f6ae8363c8b2a8a702cb1938037886
ch9729 <karlll@naver.com>
2025-02-05 10:16:57 +0900

    note controller 작성

---
+++ src/main/java/com/secure/notes/controllers/NoteController.java
@@ -0,0 +1,49 @@
+package com.secure.notes.controllers;
+
+import com.secure.notes.models.Note;
+import com.secure.notes.services.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+    @Autowired
+    private NoteService noteService;
+
+    // 노트 추가
+    @PostMapping
+    public Note createNote(@RequestBody String content, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        System.out.println("USER: " + username);
+        return noteService.createNoteForUser(username, content);
+    }
+
+    // 노트 전체 가져오기
+    @GetMapping
+    public List<Note> getUserNotes(@AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        System.out.println("USER: " + username);
+        return noteService.getNotesForUser(username);
+    }
+
+    // 노트 수정
+    @PutMapping("/{noteId}")
+    public Note updateNote(@PathVariable long noteId, @RequestBody String content, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        return noteService.updateNoteForUser(noteId, username, content);
+    }
+
+    // 노트 삭제
+    @DeleteMapping("/{noteId}")
+    public void deleteNote(@PathVariable long noteId, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        noteService.deleteNoteForUser(noteId, username);
+    }
+
+}
