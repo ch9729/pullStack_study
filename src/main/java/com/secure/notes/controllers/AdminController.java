b5018764b012dc475278fb149ccfc0810a0c8901
ch9729 <karlll@naver.com>
2025-02-06 11:32:14 +0900

    관리자 컨트롤러 생성

---
+++ src/main/java/com/secure/notes/controllers/AdminController.java
@@ -0,0 +1,42 @@
+package com.secure.notes.controllers;
+
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.User;
+import com.secure.notes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+    //유저의 목록을 가져온다.
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(),
+                HttpStatus.OK);
+    }
+
+    // 유저의 권한 수정
+    @PutMapping("/update-role")
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId,
+                                                 @RequestParam String roleName) {
+        userService.updateUserRole(userId, roleName);
+        return ResponseEntity.ok("User role updated");
+    }
+
+    // 유저의 id를 가져와 한유저의 정보 확인
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id),
+                HttpStatus.OK);
+    }
+}
