d757d493afd2afefbd13e3a42f0f89ce61ffb508
ch9729 <karlll@naver.com>
2025-02-12 10:42:59 +0900

    AuditLog 서비스 Note 서비스에 적용시키기

--- src/main/java/com/secure/notes/services/impl/NoteServiceImpl.java
+++ src/main/java/com/secure/notes/services/impl/NoteServiceImpl.java
@@ -2,6 +2,7 @@ package com.secure.notes.services.impl;
 
 import com.secure.notes.models.Note;
 import com.secure.notes.repositories.NoteRepository;
+import com.secure.notes.services.AuditLogService;
 import com.secure.notes.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,13 +15,18 @@ public class NoteServiceImpl implements NoteService {
     // 생성자 주입
     @Autowired
     private NoteRepository noteRepository;
-    
+
+    @Autowired
+    private AuditLogService auditLogService;
+
+    // 새 노트 생성
     @Override
     public Note createNoteForUser(String username, String content) {
         Note note = new Note();
         note.setContent(content);
         note.setOwnerUsername(username);
         Note savedNote = noteRepository.save(note);
+        auditLogService.logNoteCreation(username, note);    // 로그 생성
         return savedNote;
     }
 
@@ -30,12 +36,15 @@ public class NoteServiceImpl implements NoteService {
                 -> new RuntimeException("Note not found"));
         note.setContent(content);
         Note updatedNote = noteRepository.save(note);
+        auditLogService.logNoteUpdate(username, note);   // 로그 수정
         return updatedNote;
     }
 
     @Override
     public void deleteNoteForUser(Long noteId, String username) {
-        noteRepository.deleteById(noteId);
+        Note note = noteRepository.findById(noteId).orElseThrow(()->new RuntimeException("Note not found"));
+        noteRepository.delete(note);
+        auditLogService.logNoteDeletion(username, noteId);  // 로그 삭제
     }
 
     @Override
